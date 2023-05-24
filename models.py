acf5ed11f3febbad85a1db16c4671a6340f3deb4
canbekley <canbekley@gmail.com>
2023-05-25 00:09:08 +0200

    extended model objects functionality

--- models.py
+++ models.py
@@ -2,47 +2,76 @@ from __future__ import annotations
 
 from collections.abc import Iterable, Sized
 from functools import reduce
-from typing import List, Tuple, Set, Callable, Union, Sequence, Any, Dict
+from typing import List, Tuple, Set, Callable, Union, Any, Dict
 import numpy as np
 
 
 class SolutionPath(list):
     """A partial or complete solution construction, described though a sequence of Edges."""
+
+    def __init__(self, seq=()):
+        super().__init__(seq)
+
     def __lt__(self, other):
         if not isinstance(other, SolutionPath):
             return True
-        return self.distance() < other.distance()
+        if not other.at_destination():
+            return True
+        if not self.at_destination():
+            return False
+        return self.distance() <= other.distance()
+
+    def at_destination(self):
+        if not len(self):
+            return False
+        return self[-1].has_path_destination
 
     def append(self, __object: Edge) -> None:
         super().append(__object)
 
     def distance(self) -> float:
         if not len(self):
-            return 0.
+            return 0.0
         return reduce(lambda a, b: a + b, map(lambda edge: edge.length, self))
+
+    def convert_to_list_of_vertices(self):
+        origin = curr_vertex = list(filter(lambda vertex: vertex.is_origin, self[0]))[0]
+        solution_vertices = [origin]
+        for edge in self:
+            curr_vertex = edge.get_direction_vertex(curr_vertex)
+            solution_vertices.append(curr_vertex)
+        return solution_vertices
 
 
 class Ant:
     """An agent able to construct solution paths in a solution space."""
+
     def __init__(self, position: Vertex):
         self.origin_position = position
         self.position = position
         self.solution_path = SolutionPath()
 
     def traverse_edges(self, cg: ConstructionGraph, *, decision_func: Callable = None, random_seed: int = None):
-        np.random.seed(random_seed)  # for deterministic testing
+        if random_seed:
+            np.random.seed(random_seed)  # for deterministic testing
         decision_func = self._probabilistic_pathing if decision_func is None else decision_func
 
         while True:
             feasible_edges = self._get_feasible_edges(cg)
+            if not feasible_edges:
+                break
             edge_choice = decision_func(feasible_edges)
             self.solution_path.append(edge_choice)
-            self.position = edge_choice.get_destination_vertex(self.position)
+            self.position = edge_choice.get_direction_vertex(self.position)
             if self._finish_construction(cg):
                 break
 
-    def get_pheromone_delta(self, edge: Edge, delta_constant: float) -> float:
-        return 0. if not self._edge_passed(edge) else delta_constant / self.solution_path.distance()
+    def get_pheromone_delta(self, edge: Edge, delta_constant: float, problem: str) -> float:
+        if not self._edge_passed(edge):
+            return 0.0
+        if problem == "shortest_path" and not self.solution_path.at_destination():
+            return 0.0
+        return delta_constant / self.solution_path.distance()
 
     def _get_feasible_edges(self, cg: ConstructionGraph) -> Set[Edge]:
         edges = cg.vertex_connections[self.position]
@@ -58,43 +87,59 @@ class Ant:
         return edge_choice
 
     def _finish_construction(self, cg: ConstructionGraph) -> bool:
-        if cg.problem == 'traveling_salesman':
+        if cg.problem == "traveling_salesman":
             return len(self.solution_path) >= len(cg.vertex_connections)
-        if cg.problem == 'shortest_path':
-            return self.position.is_path_destination or (len(self.solution_path) >= len(cg.vertex_connections))
+        if cg.problem == "shortest_path":
+            return self.position.is_destination or (len(self.solution_path) >= len(cg.vertex_connections))
 
     def _edge_passed(self, edge: Edge) -> bool:
         return edge in self.solution_path
 
     @staticmethod
     def _get_edge_probabilities(feasible_edges: Set[Edge]) -> Dict[Edge, float]:
-        edge_qualities = {edge: edge.edge_quality for edge in list(feasible_edges)}
+        edge_qualities = {edge: edge.edge_quality for edge in sorted(list(feasible_edges))}
         edge_quality_sum = sum(edge_qualities.values())
-        edge_probabilities = {edge: edge_score / edge_quality_sum if edge_quality_sum else 1 / len(feasible_edges)
-                              for edge, edge_score in edge_qualities.items()}
+        edge_probabilities = {
+            edge: edge_score / edge_quality_sum if edge_quality_sum else 1 / len(feasible_edges)
+            for edge, edge_score in edge_qualities.items()
+        }
         return edge_probabilities
 
 
 class Vertex(tuple):
-    """Immutable n-dimensional waypoints, representing traversable points in the solution construction."""
+    """Immutable n-dimensional waypoint, representing traversable points in the solution construction."""
+
     def __new__(cls, *args, **kwargs):
         return tuple.__new__(Vertex, *args, **kwargs)
 
-    def __init__(self, *args, is_path_destination: bool = False):
+    def __init__(self, *args, info: str | None = None):
+        """Initializes an n-dimensional Vertex.
+
+        Args:
+            info: one of "origin", "destination" or None.
+        """
         super().__init__()
-        self._is_path_destination = is_path_destination
+        self._info = info
 
     def __sub__(self, other: Union[Iterable, Sized]) -> Vertex:
         """Generates a new Vertex from array subtraction."""
         if not isinstance(other, (Iterable, Sized)):
-            raise TypeError('Subtracted object is not an iterable or is not sized.')
+            raise TypeError("Subtracted object is not an iterable or is not sized.")
         if len(other) != len(self):
-            raise ValueError('Different sized arrays cannot be subtracted.')
-        return Vertex([m - n for m, n in zip(self, other)], is_path_destination=self.is_path_destination)
+            raise ValueError("Different sized arrays cannot be subtracted.")
+        return Vertex([m - n for m, n in zip(self, other)], info=self.info)
 
     @property
-    def is_path_destination(self):
-        return self._is_path_destination
+    def info(self):
+        return self._info
+
+    @property
+    def is_origin(self):
+        return self.info == "origin"
+
+    @property
+    def is_destination(self):
+        return self.info == "destination"
 
 
 class Edge:
@@ -102,16 +147,13 @@ class Edge:
 
     Contains the pheromone variable.
     """
-    def __init__(self,
-                 i: Vertex,
-                 j: Vertex,
-                 control_param_pheromone: float = 1.,
-                 control_param_distance: float = 1.):
+
+    def __init__(self, i: Vertex, j: Vertex, control_param_pheromone: float = 1.0, control_param_distance: float = 1.0):
         self.i = i
         self.j = j
         self._control_param_pheromone = control_param_pheromone
         self._control_param_distance = control_param_distance
-        self._pheromone = 0.  # type: float
+        self._pheromone = 1.0  # type: float
 
     def __contains__(self, item) -> bool:
         return item in [self.i, self.j]
@@ -120,8 +162,7 @@ class Edge:
         """Edges are non-directional, meaning Edge(i, j) == Edge(j, i)."""
         if not isinstance(other, Edge):
             return False
-        return ((self.i == other.i) and (self.j == other.j)) or (
-                (self.i == other.j) and (self.j == other.i))
+        return ((self.i == other.i) and (self.j == other.j)) or ((self.i == other.j) and (self.j == other.i))
 
     def __lt__(self, other: Any) -> bool:
         """A generic way to make Edges sortable."""
@@ -129,12 +170,16 @@ class Edge:
             return True
         return (sum(self.i) + sum(self.j)) < (sum(other.i) + sum(other.j))
 
+    def __iter__(self):
+        yield self.i
+        yield self.j
+
     def __hash__(self) -> int:
         """Sorting vertices here to assure same resulting hashes, independent of Vertex order."""
         return hash(str(sorted([str(self.i), str(self.j)])))
 
     def __repr__(self) -> str:
-        return f"Edge({str(self.i)}, {str(self.j)})"
+        return f"Edge({str(self.i)}, {str(self.j)}, tau={'{:.3f}'.format(self._pheromone)})"
 
     @property
     def pheromone(self):
@@ -151,9 +196,13 @@ class Edge:
 
     @property
     def edge_quality(self):
-        return (self.pheromone ** self._control_param_pheromone) * (self.distance_score ** self._control_param_distance)
+        return (self.pheromone**self._control_param_pheromone) * (self.distance_score**self._control_param_distance)
 
-    def get_destination_vertex(self, curr_vertex: Vertex):
+    @property
+    def has_path_destination(self):
+        return self.i.is_destination or self.j.is_destination
+
+    def get_direction_vertex(self, curr_vertex: Vertex):
         if curr_vertex == self.i:
             return self.j
         if curr_vertex == self.j:
@@ -168,20 +217,33 @@ class ConstructionGraph:
 
     Mutable Edges are supposed to be accessed and altered through this object.
     """
-    def __init__(self, *vertices: Vertex, edges: List[Edge] | None = None, problem: str = 'traveling_salesman'):
-        self.vertices = vertices  # type: Tuple[Vertex]
-        self.edges = edges if edges else self.get_all_combinatorial_edges()  # type: List[Edge]
-        self.vertex_connections = self.get_all_vertex_connections()  # type: dict
+
+    def __init__(
+        self,
+        *vertices: Vertex,
+        problem: str = "traveling_salesman",
+        edges: List[Edge] | None = None,
+        proximity: float | None = None,
+    ):
+        super().__init__()
+        self.vertices = vertices
+        if edges:
+            self.edges = edges
+        else:
+            self.edges = self.get_all_combinatorial_edges(*self.vertices, proximity=proximity)  # type: List[Edge]
+        self.vertex_connections = self.get_all_vertex_connections(*self.vertices)  # type: dict
         self.problem = problem
 
-    def get_all_combinatorial_edges(self) -> List[Edge]:
-        edges = map(lambda v1: list(map(lambda v2: Edge(v1, v2), self.vertices[self.vertices.index(v1) + 1:])),
-                    self.vertices)
-        edges_reduced = reduce(lambda a, b: a + b, edges)
-        return edges_reduced
+    @staticmethod
+    def get_all_combinatorial_edges(*vertices: Vertex, proximity: float | None = None) -> List[Edge]:
+        edges = map(lambda v1: list(map(lambda v2: Edge(v1, v2), vertices[vertices.index(v1) + 1:])), vertices)
+        edges = reduce(lambda a, b: a + b, edges)
+        if proximity:
+            edges = list(filter(lambda x: x.length <= proximity, edges))
+        return edges
 
-    def get_all_vertex_connections(self) -> dict:
-        vertex_connections = {vertex: set(filter(lambda edge: vertex in edge, self.edges)) for vertex in self.vertices}
+    def get_all_vertex_connections(self, *vertices: Vertex) -> dict:
+        vertex_connections = {v: set(filter(lambda edge: v in edge, self.edges)) for v in vertices}
         return vertex_connections
 
 
@@ -190,32 +252,40 @@ class ConstructionCycle:
 
     Generates a new optimization cycle on each next() call.
     """
-    def __init__(self,
-                 num_ants: int,
-                 ant_position: Vertex,
-                 vertices: Tuple[Vertex, ...],
-                 edges: List[Edge] | None = None,
-                 *,
-                 evaporation_rate: float = 0.1,
-                 delta_constant: float = 1.,
-                 control_param_pheromone: float = 1.,
-                 control_param_distance: float = 1.,
-                 problem: str = 'traveling_salesman'):
+
+    def __init__(
+        self,
+        num_ants: int,
+        vertices: Tuple[Vertex, ...],
+        edges: List[Edge] | None = None,
+        edge_proximity: float | None = None,
+        *,
+        evaporation_rate: float = 0.1,
+        delta_constant: float = 1.0,
+        control_param_pheromone: float = 1.0,
+        control_param_distance: float = 1.0,
+        problem: str = "traveling_salesman",
+    ):
         self.num_ants = num_ants
-        self.ant_position = ant_position
-        self.params = {'evaporation_rate': evaporation_rate,
-                       'delta_constant': delta_constant,
-                       'control_param_pheromone': control_param_pheromone,
-                       'control_param_distance': control_param_distance,
-                       'problem': problem}
-        self.construction_graph = ConstructionGraph(*vertices, edges=edges, problem=self.params['problem'])
+        self.origin_vertex = self._extract_origin_vertex(*vertices)
+        self.params = {
+            "evaporation_rate": evaporation_rate,
+            "delta_constant": delta_constant,
+            "control_param_pheromone": control_param_pheromone,
+            "control_param_distance": control_param_distance,
+            "problem": problem,
+        }
+        self.construction_graph = ConstructionGraph(
+            *vertices, problem=self.params["problem"], edges=edges, proximity=edge_proximity
+        )
+        self.ants = None  # type: List[Ant] | None
         self.solution_best_so_far = None  # type: SolutionPath | None
         self.solution_iteration_best = None  # type: SolutionPath | None
 
     def __next__(self) -> ConstructionGraph:
-        ants = [Ant(self.ant_position) for _ in range(self.num_ants)]
-        self._construct_solution(*ants)
-        self._update_pheromones(*ants)
+        self.ants = [Ant(self.origin_vertex) for _ in range(self.num_ants)]
+        self._construct_solution(*self.ants)
+        self._update_pheromones(*self.ants)
         return self.construction_graph
 
     def __iter__(self) -> ConstructionCycle:
@@ -232,6 +302,14 @@ class ConstructionCycle:
 
     def _update_pheromones(self, *ants: Ant):
         for edge in self.construction_graph.edges:
-            pheromone_deltas = map(lambda ant: ant.get_pheromone_delta(edge, self.params['delta_constant']), ants)
+            pheromone_deltas = map(
+                lambda ant: ant.get_pheromone_delta(edge, self.params["delta_constant"], self.params["problem"]), ants
+            )
             pheromone_delta_sum = sum(list(pheromone_deltas))
-            edge.calculate_new_pheromone_levels(pheromone_delta_sum, self.params['evaporation_rate'])
+            edge.calculate_new_pheromone_levels(pheromone_delta_sum, self.params["evaporation_rate"])
+
+    @staticmethod
+    def _extract_origin_vertex(*vertices: Vertex):
+        origins = list(filter(lambda v: v.is_origin, vertices))
+        if origins:
+            return origins[0]
--- tests.py
+++ tests.py
@@ -3,16 +3,16 @@ from util import fake_random_selection
 
 
 def creat_vertices():
-    v1 = Vertex([1, 2])
-    v2 = Vertex([2, 1], is_path_destination=True)
+    v1 = Vertex([1, 2], info="origin")
+    v2 = Vertex([2, 1], info="destination")
     v3 = Vertex([3, 3])
     return v1, v2, v3
 
 
 def test_vertex_is_correctly_attributed():
     vertices = creat_vertices()
-    assert vertices[0].is_path_destination is False
-    assert vertices[1].is_path_destination is True
+    assert vertices[0].info == "origin"
+    assert vertices[1].info == "destination"
 
 
 def test_edge_is_non_directional():
@@ -38,13 +38,35 @@ def test_edge_has_length():
     assert edge.length == 1
 
 
+def test_comparison_of_solution_paths():
+    e1 = Edge(Vertex([0], info="origin"), Vertex([0.5]))
+    e2 = Edge(Vertex([0.5]), Vertex([2], info="destination"))
+    e3 = Edge(Vertex([0.5]), Vertex([1], info="destination"))
+
+    sp1 = SolutionPath([e1])
+    sp2 = SolutionPath([e1, e2])
+    sp3 = SolutionPath([e1, e3])
+
+    assert min(sp1, sp2) == sp2  # sp1 didn't end on a destination vertex
+    assert min(sp2, sp3) == sp3  # sp3 has a shorter distance to a destination vertex
+
+
+def test_conversion_of_solution_path_to_list_of_vertices():
+    vertices = creat_vertices()
+    sp = SolutionPath([Edge(vertices[0], vertices[2]), Edge(vertices[2], vertices[1])])
+
+    assert sp.convert_to_list_of_vertices() == [vertices[0], vertices[2], vertices[1]]
+
+
 def test_creation_of_construction_graph():
     vertices = creat_vertices()
     cg = ConstructionGraph(*vertices)
 
-    assert cg.vertex_connections == {vertices[0]: {Edge(vertices[0], vertices[1]), Edge(vertices[0], vertices[2])},
-                                     vertices[1]: {Edge(vertices[1], vertices[0]), Edge(vertices[1], vertices[2])},
-                                     vertices[2]: {Edge(vertices[2], vertices[0]), Edge(vertices[2], vertices[1])}}
+    assert cg.vertex_connections == {
+        vertices[0]: {Edge(vertices[0], vertices[1]), Edge(vertices[0], vertices[2])},
+        vertices[1]: {Edge(vertices[1], vertices[0]), Edge(vertices[1], vertices[2])},
+        vertices[2]: {Edge(vertices[2], vertices[0]), Edge(vertices[2], vertices[1])},
+    }
 
 
 def test_creation_of_construction_graph_with_provided_edges():
@@ -52,27 +74,41 @@ def test_creation_of_construction_graph_with_provided_edges():
     edges = [Edge(vertices[0], vertices[1]), Edge(vertices[1], vertices[2])]
     cg = ConstructionGraph(*vertices, edges=edges)
 
-    assert cg.vertex_connections == {vertices[0]: {Edge(vertices[0], vertices[1])},
-                                     vertices[1]: {Edge(vertices[1], vertices[0]), Edge(vertices[1], vertices[2])},
-                                     vertices[2]: {Edge(vertices[2], vertices[1])}}
+    assert cg.vertex_connections == {
+        vertices[0]: {Edge(vertices[0], vertices[1])},
+        vertices[1]: {Edge(vertices[1], vertices[0]), Edge(vertices[1], vertices[2])},
+        vertices[2]: {Edge(vertices[2], vertices[1])},
+    }
+
+
+def test_creation_of_construction_graph_based_on_vertex_proximity():
+    vertices = [Vertex([0]), Vertex([2]), Vertex([4])]
+    proximity = 2.5
+    cg = ConstructionGraph(*vertices, proximity=proximity)
+
+    assert cg.vertex_connections == {
+        vertices[0]: {Edge(vertices[0], vertices[1])},
+        vertices[1]: {Edge(vertices[1], vertices[0]), Edge(vertices[1], vertices[2])},
+        vertices[2]: {Edge(vertices[2], vertices[1])},
+    }
 
 
 def test_ant_traverses_all_edges_in_traveling_salesman():
     vertices = creat_vertices()
-    construction_graph = ConstructionGraph(*vertices, problem='traveling_salesman')
+    construction_graph = ConstructionGraph(*vertices, problem="traveling_salesman")
     ant = Ant(vertices[0])
 
     # inject fake random choice function to get deterministic behavior for testing
     ant.traverse_edges(construction_graph, decision_func=fake_random_selection)
 
-    assert ant.solution_path == SolutionPath([Edge(vertices[0], vertices[1]),
-                                              Edge(vertices[1], vertices[2]),
-                                              Edge(vertices[2], vertices[0])])
+    assert ant.solution_path == SolutionPath(
+        [Edge(vertices[0], vertices[1]), Edge(vertices[1], vertices[2]), Edge(vertices[2], vertices[0])]
+    )
 
 
 def test_ant_finishes_construction_in_shortest_path():
     vertices = creat_vertices()
-    construction_graph = ConstructionGraph(*vertices, problem='shortest_path')
+    construction_graph = ConstructionGraph(*vertices, problem="shortest_path")
     ant = Ant(vertices[0])
 
     ant.traverse_edges(construction_graph, decision_func=fake_random_selection)
@@ -81,21 +117,35 @@ def test_ant_finishes_construction_in_shortest_path():
 
 def test_pheromones_placed_after_iteration():
     vertices = creat_vertices()
-    cycle = ConstructionCycle(2, vertices[0], vertices)
+    cycle = ConstructionCycle(2, vertices)
 
     construction_graph = next(cycle)
     assert construction_graph.edges[0].pheromone > 0
 
 
 def test_pheromones_placed_with_correct_evaporation():
-    vertices = (Vertex([1]), Vertex([2]), Vertex([3]))  # 1-d vertices, distance of every round-trip should always be 4
-    cycle = ConstructionCycle(2, vertices[0], vertices)  # 2 ants have a combined pheromone delta of 0.5 ( 1/4 + 1/4 )
+    vertices = (
+        Vertex([1], info="origin"),
+        Vertex([2]),
+        Vertex([3]),
+    )  # 1-d vertices, distance of every round-trip should always be 4
+    cycle = ConstructionCycle(2, vertices, evaporation_rate=0.1)
 
-    construction_graph = next(cycle)  # starting with 0. pheromone, every edge should now have 0.5 pheromone
-    assert all([edge.pheromone == 0.5 for edge in construction_graph.edges])
+    construction_graph = next(cycle)  # with 0.1 being evaporated, every edge should now have 1.4 pheromone
+    assert all([edge.pheromone == 1.4 for edge in construction_graph.edges])
 
-    construction_graph = next(cycle)  # 0.05 being evaporated, every edge should now have 0.95 pheromone
-    assert all([edge.pheromone == 0.95 for edge in construction_graph.edges])
+    construction_graph = next(cycle)  # with 0.14 being evaporated, every edge should now have 0.95 pheromone
+    assert all([edge.pheromone == 1.76 for edge in construction_graph.edges])
+
+
+def test_pheromones_not_added_when_not_reaching_destination():
+    # having a shortest-path problem, but no destination vertex defined. expect every edge to lose pheromone.
+    vertices = (Vertex([1], info="origin"), Vertex([2]), Vertex([3]))
+    cycle = ConstructionCycle(1, vertices, evaporation_rate=0.1, problem="shortest_path")
+
+    construction_graph = next(cycle)
+    # initial pheromone level 1.0, subtracted by the evaporation of 0.1
+    assert all([edge.pheromone == 0.9 for edge in construction_graph.edges])
 
 
 def test_correct_edge_probabilities_applied():
@@ -106,30 +156,14 @@ def test_correct_edge_probabilities_applied():
     feasible_edges = ant._get_feasible_edges(construction_graph)
     assert feasible_edges == {Edge(Vertex([1]), Vertex([2])), Edge(Vertex([1]), Vertex([3]))}
 
-    for edge in feasible_edges:
-        edge._pheromone = 1.
     edge_probabilities = ant._get_edge_probabilities(feasible_edges)
     assert edge_probabilities[Edge(Vertex([1]), Vertex([2]))] == 2 / 3
     assert edge_probabilities[Edge(Vertex([1]), Vertex([3]))] == 1 / 3
 
 
-def test_pheromones_placed_with_shortest_path_problem():
-    vertices = (Vertex([1, 1]), Vertex([3, 1]), Vertex([1, 3]), Vertex([3, 3]))
-    # custom edges with no connection between vertex(3, 1) and vertex(1, 3)
-    edges = [Edge(vertices[0], vertices[1]), Edge(vertices[0], vertices[2]),
-             Edge(vertices[1], vertices[3]), Edge(vertices[2], vertices[3])]
-    cycle = ConstructionCycle(2, vertices[0], vertices, edges, problem='shortest_path')
-
-    construction_graph = next(cycle)  # starting with 0. pheromone, every edge should now have 0.5 pheromone
-    assert all([edge.pheromone == 0.25 for edge in construction_graph.edges])
-
-
 def test_saving_of_iteration_best_and_best_so_far_solutions():
-    v = (Vertex([1]),
-         Vertex([2], is_path_destination=True),
-         Vertex([3]),
-         Vertex([4]))
-    cycle = ConstructionCycle(2, v[0], v)
+    v = (Vertex([1], info="origin"), Vertex([2], info="destination"), Vertex([3]), Vertex([4]))
+    cycle = ConstructionCycle(2, v)
     ants = [Ant(v[0]) for _ in range(2)]
 
     ants[0].solution_path = best_path = SolutionPath([Edge(v[0], v[1])])
